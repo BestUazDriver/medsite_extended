35080b93e30fd317d08b23165db5b166d5f7afc3
DDD <Sabitov.daniyar18@mail.ru>
2022-03-20 17:07:30 +0300

    Project base state

---
+++ src/main/java/com/example/medsite_extended/MedsiteExtendedApplication.java
@@ -0,0 +1,13 @@
+package com.example.medsite_extended;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsiteExtendedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedsiteExtendedApplication.class, args);
+    }
+
+}
